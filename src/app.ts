49d077ed23a26687ac263b3488525b8a1782cdc1
BeahIF <beatrizdacosta1@gmail.com>
2021-10-05 11:15:07 -0300

    finalizando curso

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+// const express = require('express')
+import express, { Request, Response, NextFunction } from 'express'
+import { json } from 'body-parser'
+import todoRoutes from './routes/todos'
+
+const app = express();
+app.use(json())
+app.use('/todos',todoRoutes)
+app.use((error:Error, req:Request,res:Response,next:NextFunction)=>{})
+app.listen(3000)
